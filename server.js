970c82ae0aa4481d3fffeb7a5fa7ddb3d5dfbe5f
martia86 <martia86>
2020-09-04 15:04:22 +0430

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const https = require("https");
 var code = ["new", "1"];
 var updated = "";
 console.log("YES");
-try{client.login("NzQ5MTMzNDgzNzI0NjM2MjQw.X0ni7A.EZFmzI9LdeH4gWPEFufYEtn52kc");}catch(err){console.log(err);}
+try{client.login(process.env.token);}catch(err){console.log(err);}
 const restart = require("./server.js");
 function execute() {
   const restart = require("./server.js");
