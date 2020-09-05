2d518a0cfd10ab8b1e1e395ef68b732b80d0da9d
martia86 <martia86>
2020-09-05 16:00:48 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 var express = require('express');
 var app = new express();
-app.use(express.json());
-app.use(express.static(__dirname + '/public'));
-     var entries = [
-        {"id":1, "title":"Hello World!"},
-        {"id":2, "title":"Hello World!"}
-        {"id":3, "title":"Hello World!"}
-        {"id":4, "title":"Hello World!"}
-        ];
-if(entries.id == varGetFromFrontend){
-console.log("This is to print a variable by choosing it from Front End")
- console.log(varGetFromFrontend)
-}
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
