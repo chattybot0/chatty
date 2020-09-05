49503b79d4433292066b55cf5bb33b55147a9865
martia86 <martia86>
2020-09-05 16:06:20 +0430

    Updated index.html and index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-var express = require('express');
-var app = new express();
-var port = Number(process.env.PORT || 5000);
-app.listen(port);
+var express = require("express");
+var http = require("http");
+var socketIO = require("socket.io");
+
+var app = express();
+app.get("/", function(req, res){
+    res.sendfile("./index.html");
+});
+
+var server = http.createServer(app);
+var io = socketIO.listen(server, {log: false});
+io.sockets.on("connection", function(socket){
+    socket.on("sendVar", function(value){
+        console.log("The value of the variable is " + value);
+    });
+});
+
+server.listen(5000);
